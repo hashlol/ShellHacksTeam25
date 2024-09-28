516bcddea50ed4509068a98243b49f4d92aee24c
SalmanNA <lordofallbutter@gmail.com>
2024-09-28 17:18:56 -0400

    flask api

--- server/src/search.py
+++ server/src/search.py
@@ -6,6 +6,11 @@ import json
 import tiktoken
 import openai
 import time
+from dotenv import load_dotenv
+import os
+load_dotenv()
+api_key_openAi = os.getenv('OPEN_AI_KEY')
+api_key_youtube  = os.getenv('YOUTUBE_API_KEY')
 
 # Your existing classes remain unchanged
 
@@ -35,7 +40,7 @@ class YouTubeSearcher:
                 video_ids = [item['id']['videoId'] for item in response['items'] if 'videoId' in item['id']]
                 for video_id in video_ids:
                     video_details = YouTubeSearcher.get_video_details(api_key, video_id)
-                    if video_details and YouTubeSearcher.is_duration_less_than_three_hours(video_details['contentDetails']['duration']):
+                    if video_details and YouTubeSearcher.is_duration_less_than_one_hours(video_details['contentDetails']['duration']):
                         return video_details
                 page_token = response.get('nextPageToken')
                 if not page_token:
@@ -59,7 +64,7 @@ class YouTubeSearcher:
             return None
 
     @staticmethod
-    def is_duration_less_than_three_hours(duration):
+    def is_duration_less_than_one_hours(duration):
         hours = 0
         minutes = 0
         if "H" in duration:
@@ -67,7 +72,7 @@ class YouTubeSearcher:
         if "M" in duration:
             minutes = int(duration[duration.index("H")+1:duration.index("M")]) if "H" in duration else int(duration[2:duration.index("M")])
         total_minutes = hours * 60 + minutes
-        return total_minutes < 180
+        return total_minutes < 60
 
 class TranscriptProcessor:
     def __init__(self):
@@ -94,27 +99,35 @@ class TranscriptProcessor:
 class OpenAIChat:
     def __init__(self, api_key):
         self.api_key = api_key
-        openai.api_key = ""
-        self.conversation_history = []
-
-    def chat_with_openai(self, user_input):
+        openai.api_key = self.api_key
+        self.conversation_history = []   
+        
+    def chat_with_openai(self,user_input):       
+        # Append user input to the conversation history
         self.conversation_history.append({"role": "user", "content": user_input})
+        
+        # Create a request with the full conversation history
         response = openai.ChatCompletion.create(
-            model="gpt-4",
+            model="gpt-4o-mini",
             messages=self.conversation_history,
-            max_tokens=2000
+            max_tokens=2000  # Adjust as needed
         )
+        
+        # Get the model's reply
         assistant_reply = response['choices'][0]['message']['content']
+        
+        # Append the assistant's reply to the conversation history
         self.conversation_history.append({"role": "assistant", "content": assistant_reply})
+        
         return assistant_reply
 
 # Create Flask app
 app = Flask(__name__)
 cors = CORS(app, origins=['*'])
-
+OpenAI = OpenAIChat(api_key_openAi)
 @app.route('/getMetaData/<query>', methods=['GET'])
 def getMetadata(query):
-    youtube_api_key = ""
+    youtube_api_key = api_key_youtube
     query = query
     
     # Step 1: Search for YouTube video
@@ -136,7 +149,7 @@ def getMetadata(query):
         
         # Example conversation
     user_input = f"The total length of the content that I want to send you is too large to send in only one piece.For sending you that content, I will follow this rule:[START PART 1/{len(chunks)}]this is the content of the part 1 out of {len(chunks)} in total[END PART 1/{len(chunks)}]Then you just answer: \"Received part 1/{len(chunks)}\"And when I tell you \"ALL PARTS SENT\", then you can continue processing the data and answering my requests. The data I am going to send is the transcript of a educational youtube video with the timestamp in seconds."
-    reply = OpenAIChat.chat_with_openai(user_input)
+    reply = OpenAIChat.chat_with_openai(OpenAI,user_input)
     print("Assistant:", reply)
 
     for i in range(len(chunks)):
@@ -145,7 +158,7 @@ def getMetadata(query):
         while True:  # Loop until the chunk is successfully processed
             try:
                 # Call the OpenAI API
-                reply = OpenAIChat.chat_with_openai(user_input)
+                reply = OpenAIChat.chat_with_openai(OpenAI,user_input)
                 print("Assistant:", reply)
                 break  # If successful, break out of the loop
             except openai.error.RateLimitError:
@@ -153,25 +166,21 @@ def getMetadata(query):
                 print(f"Rate limit exceeded. Waiting 1 second before retrying chunk {i+1}.")
                 time.sleep(1)  # Wait for 1 second before retrying
 
-            user_input = f"ALL PARTS SENT"
-            reply = OpenAIChat.chat_with_openai(user_input)
+    user_input = f"ALL PARTS SENT"
+    reply = OpenAIChat.chat_with_openai(OpenAI,user_input)
 
 
-            json_string = '{"titles":[{"title":"Title 1","timestamp":"Time in seconds for the start of section","flashcards":[{"term":"Term 1.1","definition":"Definition of term 1.1"},{"term":"Term 1.2","definition":"Definition of term 1.2"}],"multiple_choice":{"question":"Question 1 for Title 1","choices":["Choice A","Choice B","Choice C","Choice D"],"answer":"Choice A"}},{"title":"Title 2","timestamp":"Time in seconds for the start of section","flashcards":[{"term":"Term 2.1","definition":"Definition of term 2.1"},{"term":"Term 2.2","definition":"Definition of term 2.2"},{"term":"Term 2.3","definition":"Definition of term 2.3"}],"multiple_choice":{"question":"Question 1 for Title 2","choices":["Choice A","Choice B","Choice C","Choice D"],"answer":"Choice A"}},{"title":"Title 3","timestamp":"Time in seconds for the start of section","flashcards":[{"term":"Term 3.1","definition":"Definition of term 3.1"}],"multiple_choice":{"question":"Question 1 for Title 3","choices":["Choice A","Choice B","Choice C","Choice D"],"answer":"Choice A"}}]}'
+    json_string = '{"titles":[{"title":"Title 1","timestamp":"Time in seconds for the start of section","flashcards":[{"term":"Term 1.1","definition":"Definition of term 1.1"},{"term":"Term 1.2","definition":"Definition of term 1.2"}],"multiple_choice":{"question":"Question 1 for Title 1","choices":["Choice A","Choice B","Choice C","Choice D"],"answer":"Choice A"}},{"title":"Title 2","timestamp":"Time in seconds for the start of section","flashcards":[{"term":"Term 2.1","definition":"Definition of term 2.1"},{"term":"Term 2.2","definition":"Definition of term 2.2"},{"term":"Term 2.3","definition":"Definition of term 2.3"}],"multiple_choice":{"question":"Question 1 for Title 2","choices":["Choice A","Choice B","Choice C","Choice D"],"answer":"Choice A"}},{"title":"Title 3","timestamp":"Time in seconds for the start of section","flashcards":[{"term":"Term 3.1","definition":"Definition of term 3.1"}],"multiple_choice":{"question":"Question 1 for Title 3","choices":["Choice A","Choice B","Choice C","Choice D"],"answer":"Choice A"}}]}'
 
 
-            user_input = f"Based on the data I have just provided, please identify and extract key sections from this video transcript. For each section, provide clear, descriptive titles along with precise timestamps (start and end times). Ensure that all relevant portions of the video are covered without skipping over significant content, and structure the sections logically for ease of navigation. Return only a title and its timestamps. Also for each sections provide 3 flashcards on the most important terms and 1 multiple choice question per title with 4 choices that are derrived from the transcipt provided. Follow this json format for the output {json_string} and return only the json"
-            reply = OpenAIChat.chat_with_openai(user_input)
+    user_input = f"Based on the data I have just provided, please identify and extract key sections from this video transcript. For each section, provide clear, descriptive titles along with precise timestamps (start and end times). Ensure that all relevant portions of the video are covered without skipping over significant content, and structure the sections logically for ease of navigation. Return only a title and its timestamps. Also for each sections provide 3 flashcards on the most important terms and 1 multiple choice question per title with 4 choices that are derrived from the transcipt provided. Follow this json format for the output {json_string} and return only the json"
+    reply = OpenAIChat.chat_with_openai(OpenAI,user_input)
+    print(reply)
             
-            
-        # Step 5: Send the JSON response with transcript chunks
-        return jsonify({
-            "videoId": video_id,
-            "json": reply
-        })
-    else:
-        print("first video", first_video)
-        return jsonify({"error": "No video found"}), 404
-
+    # Step 5: Send the JSON response with transcript chunks
+    return jsonify({
+        "videoId": video_id,
+        "json": reply
+    })
 if __name__ == '__main__':
     app.run(debug=True)
